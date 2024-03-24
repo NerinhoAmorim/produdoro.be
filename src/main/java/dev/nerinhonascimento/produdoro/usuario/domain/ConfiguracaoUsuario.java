4acddcf0ccf3dd58224de0a7aa2785fb0c69a1d1
Nerinho Nascimento <neramornas@gmail.com>
2024-03-24 00:35:11 -0300

    Adicionar banco de dados e modela agregados de usuario e configuracao e credencial

---
+++ src/main/java/dev/nerinhonascimento/produdoro/usuario/domain/ConfiguracaoUsuario.java
@@ -0,0 +1,16 @@
+package dev.nerinhonascimento.produdoro.usuario.domain;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder(access = AccessLevel.PACKAGE)
+@Getter	
+public class ConfiguracaoUsuario {
+	private Integer tempoMinutosFoco;
+	private Integer tempoMinutosPausaCurta;
+	private Integer tempoMinutosPausaLonga;
+	private Integer repeticoesParaPausaLonga;
+
+
+}
---
+++ src/main/java/dev/nerinhonascimento/produdoro/usuario/domain/StatusUsuario.java
@@ -0,0 +1,6 @@
+package dev.nerinhonascimento.produdoro.usuario.domain;
+
+public enum StatusUsuario {
+	FOCO, PAUSA_CURTA, PAUSA_LONGA;
+
+}
