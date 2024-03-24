f1400e7a2f7fdd9e7d9b9d79787af2aea3874d2d
Nerinho Nascimento <neramornas@gmail.com>
2024-03-24 15:30:24 -0300

    mapeia agregado de tarefa

---
+++ src/main/java/dev/nerinhonascimento/produdoro/tarefa/domain/StatusTarefa.java
@@ -0,0 +1,5 @@
+package dev.nerinhonascimento.produdoro.tarefa.domain;
+
+public enum StatusTarefa {
+
+}
---
+++ src/main/java/dev/nerinhonascimento/produdoro/tarefa/domain/Tarefa.java
@@ -0,0 +1,30 @@
+package dev.nerinhonascimento.produdoro.tarefa.domain;
+
+import java.util.UUID;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@Document(collection = "Projeto")
+public class Tarefa {
+	@Id
+	private UUID idProjeto;
+	private String nome;
+	private String descricao;
+	@Indexed
+	private UUID idUsuario;
+	@Indexed
+	private UUID idArea;
+
+}
